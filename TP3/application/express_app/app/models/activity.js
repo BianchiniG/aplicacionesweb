9dd59894d0aa4473d0bb892c41e8d18a26ca4eef
German Bianchini <bianchiniger@gmail.com>
2019-06-20 18:31:20 -0300

    Creada la clase dentro del modelo de actividades con un par de metodos de instancia para hacer validacion de tags y actualizacion de datos

--- TP3/application/express_app/app/models/activity.js
+++ TP3/application/express_app/app/models/activity.js
@@ -27,6 +27,29 @@ var activitySchema = mongoose.Schema({
     }
 });
 
+class ActivityClass {
+    hasTag(tag_id) {
+        var has = false;
+        this.tags.forEach(tag => {
+            if (tag == tag_id) {
+                has = true;
+            }
+        });
+        return has;
+    }
+
+    updateData(data) {
+        this.title = data.title;
+        this.short_description = data.short_description;
+        this.long_description = data.long_description;
+        if (data.tags.length) {
+            // Add the new tags if they exist and are not already present on the array.
+        }
+        this.coordinates = data.coordinates;
+        this.save();
+        return true;
+    }
+}
 var Activity = module.exports = mongoose.model('activity', activitySchema);
 module.exports.get = function (callback, limit) {
     Activity.find(callback).limit(limit);
